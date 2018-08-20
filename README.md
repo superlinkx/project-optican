237b4b7b2fbce45733d73fe5af3f2920eba2af48
Steven Holms <superlinkx@gmail.com>
2018-08-20 00:12:06 -0600

    Updated Readme Added basic GORM testing Schema in progress

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # project-healthpack
 Personal API project for tracking health stats
 
+# Getting Started
+You'll need to create a .env file for local development options. The variables needed are:
+```
+DB_USER=healthpack
+DB_PASSWORD=healthpack
+DB_HOST=localhost
+APP_PORT=8080
+```
+Set as appropriate, then run the project
+
 # Running the Project
+Running docker compose will create two containers, one for the development database and one for the api
+The api is available on port `3000` and the database is exposed for development purposes on port `5432`
 Run `docker-compose up`
+
+Once the docker containers are running, it will be possible to use regular go debugging techniques
+while connected to the database. This includes using `go run`, but it will be available on whatever `APP_PORT`
+you've chosen
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 	"time"
@@ -13,26 +14,63 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+// Record Entity used to describe a health record
+type Record struct {
+	gorm.Model
+	RecordTypeID uint
+	RecordType   RecordType `json:"record_type"`
+}
+
+// RecordType Entity used for describing types of records
+type RecordType struct {
+	gorm.Model
+	Name  string `json:"name"`
+	Units string `json:"units"`
+}
+
 func main() {
-
-	cs := fmt.Sprintf("host=%s user=%s password=%s dbname=healthpack sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
-	r := gin.Default()
-
-	db, err := gorm.Open("postgres", cs)
-	for err != nil {
-		time.Sleep(time.Second)
-		glog.Error("Couldn't connect due to error: ", err)
-		db, err = gorm.Open("postgres", cs)
+	setDB()
+	gormdb, err := gorm.Open("postgres", db)
+	if err != nil {
+		glog.Error("Gorm failed to connect: ", err)
 	}
-	defer db.Close()
+	runMigrations(gormdb)
+
+	var recordType RecordType
+	var record []Record
+	gormdb.Create(&RecordType{Name: "Name", Units: "em"})
+	gormdb.First(&recordType, 1)
+	gormdb.Create(&Record{RecordType: recordType})
+	gormdb.Preload("RecordType").Find(&record)
+
+	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message":        "pong",
-			"error":          err,
-			"connection_str": cs,
+			"message":     "pong",
+			"record_type": recordType,
+			"record":      record,
 		})
 	})
 	r.Run(":" + os.Getenv("APP_PORT"))
+}
+
+func setDB() {
+	var err error
+	cs := fmt.Sprintf("host=%s user=%s password=%s dbname=healthpack sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+	db, err = sql.Open("postgres", cs)
+	for err != nil {
+		time.Sleep(time.Second)
+		glog.Error("Couldn't connect due to error: ", err)
+		db, err = sql.Open("postgres", cs)
+	}
+}
+
+func runMigrations(gormdb *gorm.DB) {
+	gormdb.AutoMigrate(&RecordType{})
+	gormdb.AutoMigrate(&Record{})
+	gormdb.Model(&Record{}).AddForeignKey("record_type_id", "record_types(id)", "RESTRICT", "RESTRICT")
 }
